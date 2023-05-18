91ab228d03bdf3fab80030c08a0651fdfba54639
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2023-05-18 11:18:52 -0500

    Comentarios al código

--- src/practica1/Familia.java
+++ src/practica1/Familia.java
@@ -3,11 +3,11 @@ package practica1;
 public class Familia {
   private Persona [] family= new Persona[4];
 
-  public Familia(Persona persona1,Persona persona2,Persona persona3,Persona persona4 ) {
-    family[0]=persona1;
-    family[1]=persona2;
-    family[2]=persona3;
-    family[3]=persona4;
+  public Familia(Persona persona1, Persona persona2, Persona persona3, Persona persona4 ) {
+    family[0] = persona1;
+    family[1] = persona2;
+    family[2] = persona3;
+    family[3] = persona4;
   }
 
   public Persona[] getFamily() {
@@ -20,12 +20,9 @@ public class Familia {
 
   public void mostrarFamilia() {
     for (int i = 0; i < 4; i++)
-      if (family[i].getName().equals("")){
-        System.out.print("");
-      }else{
+      if (!family[i].getName().equals("")) {
         System.out.println(family[i].toString());
       }
-
   }
 
 
--- src/practica1/Persona.java
+++ src/practica1/Persona.java
@@ -1,36 +1,38 @@
+//Creamos esta clase para simbolizar a cada tripulante de la nave, con los atributos necesarios para el ejercicio
+
 package practica1;
 
 public class Persona {
-  private String name;
-  private String gender;
+  private String nombre;
+  private String genero;
   private int edad;
 
   public Persona(String name, String gender, int edad) {
-    this.name = name;
-    this.gender = gender;
+    this.nombre = name;
+    this.genero = gender;
     this.edad = edad;
   }
 
   public Persona() {
-    name = "";
-    gender = "";
+    nombre = "";
+    genero = "";
     edad = -1;
   }
 
   public String getName() {
-    return name;
+    return nombre;
   }
 
   public void setName(String name) {
-    this.name = name;
+    this.nombre = name;
   }
 
   public String getGender() {
-    return gender;
+    return genero;
   }
 
   public void setGender(String gender) {
-    this.gender = gender;
+    this.genero = gender;
   }
 
   public int getEdad() {
@@ -44,8 +46,8 @@ public class Persona {
   @Override
   public String toString() {
     return "Persona{" +
-            "name='" + name + '\'' +
-            ", gender='" + gender + '\'' +
+            "name='" + nombre + '\'' +
+            ", gender='" + genero + '\'' +
             ", edad=" + edad +
             '}';
   }
